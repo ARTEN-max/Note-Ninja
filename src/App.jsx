aee895e9045ef053330cdd360ab259c3cf2433c9
Abdul Rahman <arlnu@uwaterloo.ca>
2025-06-09 10:15:30 +0400

    Make sidebar responsive: hide on mobile, show on tablet/desktop

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function AppContent() {
   return (
     <div style={{ display: "flex", minHeight: "100vh", width: "100vw", background: "linear-gradient(to bottom, rgb(251, 207, 232), rgb(249, 168, 212))" }}>
       {showSidebar && <Sidebar showMinimize={showMinimize} />}
-      <div className={showSidebar ? 'w-full md:ml-[240px]' : 'w-full'} style={{ width: '100%' }}>
+      <div className={showSidebar ? 'w-full md:ml-[240px] transition-all duration-300' : 'w-full'} style={{ width: '100%' }}>
         <Routes>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/register" element={<Register />} />
